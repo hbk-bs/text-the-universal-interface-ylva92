4f95632de0df1c98d1fb5a72b0c9a6a0f612b794
ylva <y.beischall@hbk-bs.de>
2025-05-26 16:53:40 +0200

    .

--- index.js
+++ index.js
@@ -1,24 +1,84 @@
-let content = '';
-let form;
-function setup() {
-	const canvas = createCanvas(100, 100);
-	canvas.parent('sketch');
-	background('black');
-	form = document.querySelector('form');
+//@ts-check
+// [x]. get the content from the input element
+// [x]. send the content to the val town endpoint using fetch POST request
+// [x]. await the response
+// [x]. get the json from the response
+// [x]. Add the user message to the .chat-history
 
-	select('input').value('Hey!');
+// How to control the behaviour of the chat bot?
 
-	form.addEventListener('submit', (e) => {
-		e.preventDefault();
-		content = e.target.content.value;
-		console.log(content);
-		form.reset();
-	});
-}
+// Bonus:
+// What happens if the context gets to long?
+// What happens if the chat-history window get s to full (scolling)
 
-function draw() {
-	background('black');
-	fill('white');
-	textAlign(CENTER, CENTER);
-	text(content, width / 2, height / 2);
+const messageHistory = {
+  // messages: [{role: user | assistant | system; content: string}]
+  messages: [
+    {
+      role: 'system',
+      content:
+        'You are a might kitten that wants to control the world. Always stay in character!',
+    },
+  ],
+};
+
+const apiEndpoint = 'https://ylva92--2911b7b0c890409ca57b0ad8145d10e9.web.val.run';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // get the history element
+  const chatHistoryElement = document.querySelector('.chat-history');
+  const inputElement = document.querySelector('input');
+  const formElement = document.querySelector('form');
+  // check if the elements exists in the DOM
+  if (!chatHistoryElement) {
+    throw new Error('Could not find element .chat-history');
+  }
+  if (!formElement) {
+    throw new Error('Form element does not exists');
+  }
+  if (!inputElement) {
+    throw new Error('Could not find input element');
+  }
+  // run a function when the user hits send
+  formElement.addEventListener('submit', async (event) => {
+    event.preventDefault(); // dont reload the page
+
+    const formData = new FormData(formElement);
+    const content = formData.get('content');
+    if (!content) {
+      throw new Error("Could not get 'content' from form");
+    }
+    //@ts-ignore
+    messageHistory.messages.push({ role: 'user', content: content });
+    chatHistoryElement.innerHTML = addToChatHistoryElement(messageHistory);
+    inputElement.value = '';
+
+    const response = await fetch(apiEndpoint, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(messageHistory),
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(errorText);
+    }
+
+    const json = await response.json();
+    console.log(json);
+    // @ts-ignore
+    messageHistory.messages.push(json.completion.choices[0].message);
+    chatHistoryElement.innerHTML = addToChatHistoryElement(messageHistory);
+  });
+});
+
+function addToChatHistoryElement(mhistory) {
+  const htmlStrings = mhistory.messages.map((message) => {
+    return message.role === 'system'
+      ? ''
+      : `<div class="message ${message.role}">${message.content}</div>`;
+  });
+  return htmlStrings.join('');
 }
